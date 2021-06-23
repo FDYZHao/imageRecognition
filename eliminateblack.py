f7827255e1ee66a3f188f6a066be8ae62d464cbf
chocolate912 <74696552+chocolate912@users.noreply.github.com>
2021-06-23 16:54:18 +0800

    Add files via upload

---
+++ eliminateblack.py
@@ -0,0 +1,13 @@
+from PIL import Image 
+import cv2
+img2 = Image.open('tb.png')
+#img1 = Image.open('./Amazing_RGB_2L.bmp')
+# img1 = img1.convert('RGBA')
+img2 = img2.convert('RGBA')
+pixdata = img2.load()
+for y in range(img2.size[1]):
+    for x in range(img2.size[0]):
+        if pixdata[x,y][0]==0 and pixdata[x,y][1]==0 and pixdata[x,y][2]==0:
+           pixdata[x, y] = (255, 255, 255,0)
+
+img2.show()
